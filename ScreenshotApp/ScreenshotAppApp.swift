c7a373f55758aaab95dd2390f6456f31969d8367
Gabriel <lizhenjiang.xidian@foxmail.com>
2025-01-06 14:24:58 +0800

    Initial Commit

---
+++ ScreenshotApp/ScreenshotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotAppApp.swift
+//  ScreenshotApp
+//
+//  Created by 李振江 on 2025/1/6.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
